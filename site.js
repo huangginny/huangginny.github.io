bb72c2b8683666815c89699852373026ff9644b4
Ginny Huang <sflsginny@hotmail.com>
2014-12-10 23:54:27 -0500

    knockout.js

--- site.js
+++ site.js
@@ -1,5 +1,36 @@
 $(function(){
 
+	/* Social network */
+	var SNS_LINKS = [
+		{
+			'name': 'Facebook',
+			'url': 'https://www.facebook.com/orz.zai',
+			'icon': '<i class="fa fa-facebook-square fa-2x"></i>'
+		},
+		{
+			'name': 'Twitter',
+			'url': 'https://twitter.com/intent/tweet?screen_name=Orz_zai',
+			'icon': '<i class="fa fa-twitter fa-2x">'
+		},
+		{
+			'name': 'LinkedIn',
+			'url': 'http://www.linkedin.com/in/jh92710',
+			'icon': '<i class="fa fa-linkedin-square fa-2x"></i>'
+		},
+		{
+			'name': 'GitHub',
+			'url': 'https://github.com/huangginny',
+			'icon': '<i class="fa fa-github fa-2x"></i>'
+		},
+		{
+			'name': 'Flickr',
+			'url': 'http://www.flickr.com/photos/orz_zai/',
+			'icon': '<i class="fa fa-flickr fa-2x"></i>'
+		},
+	];
+	ko.applyBindings({'links': SNS_LINKS});
+
+	/* WHOLE PAGE */
 	$(".section").each(function() {
 		var head = $(this).children('.sectionHead');
 		var body = $(this).children('.sectionBody');
