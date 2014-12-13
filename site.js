26f122cf478213eeff4dc6a6da968f2bde909edc
Ginny Huang <sflsginny@hotmail.com>
2014-12-13 11:23:50 -0500

    Animation that makes better sense

--- site.js
+++ site.js
@@ -1,178 +1,21 @@
 $(function(){
 
-	/* Timeline */
-	var TIMELINE = [
-		{
-			'time': '2014.9',
-			'event': 'Starts internship at Center For Open Science in Charlottesville, VA',
-			'milestone': true,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2014.5',
-			'event': 'Graduates from Washington and Lee University',
-			'milestone': true,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2013.5 - 2013.8',
-			'event': 'Works on Android Automated UI Testing in AmLogic, Inc in '
-				+ 'Santa Clara, CA',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2013.4',
-			'event': 'Goes to Cuba with Christy Cui',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2012.6 - 2012.8',
-			'event': 'Researches on properties of fibonacci numbers as an'
-				+ ' R.E.Lee Scholar, with Professor Dresden and my classmate Cathy',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2011.10',
-			'event': 'Starts work study at Phonathon',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2011.7',
-			'event': 'Movie <i>Harry Potter and the Deathly Hallows - Part 2</i> '
-				+ 'is published, marking the end of the Potter era',
-			'milestone': true,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2011.4',
-			'event': 'Starts my first solo trip in Washington D.C',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2010.8',
-			'event': 'Leaves for United States, and enrolls in Washington and Lee University in Lexington, VA',
-			'milestone': true,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2010.6',
-			'event': 'Graduates from Shenzhen Foreign Languages School',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2009.3',
-			'event': 'Gets 102 on my first TOEFL',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2008.3',
-			'event': 'Co-direct my first short film, <i>Illusion</i>, which '
-				+ 'turns out to be a disaster',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2007.7',
-			'event': 'The book <i>Harry Potter and the Deathly Hallows</i> is published',
-			'milestone': true,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2007.7',
-			'event': 'Screws up my high school entrance exam',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2004.9',
-			'event': 'Starts middle school in Shenzhen Foreign Languages School',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2004.6',
-			'event': 'Graduates from elementary school',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '2004.6',
-			'event': 'Passes the test that I have performed most outstandingly in my life',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '1999.?',
-			'event': "Reads <i>Harry Potter and the Philosopher's Stone</i>",
-			'milestone': true,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '1998.9',
-			'event': 'Starts elementary school',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '1997.?',
-			'event': 'Talks',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '1994.?',
-			'event': 'Walks',
-			'milestone': false,
-			'details': ko.observableArray([])
-		},
-		{
-			'time': '1992.7',
-			'event': 'Born',
-			'milestone': true,
-			'details': ko.observableArray([])
-		}
-	];
+	/* --- HEADER --- */
+	$("#head button").each(function() {
+		$(this).click(function() {
+			var name = $(this).attr('name');
+			var sectionId = "#" + name;
+			$('html, body').animate({
+        		scrollTop: $(sectionId).offset().top
+    		});
+    		$(sectionId).children('.sectionBody').show();
+		});
+	});
 
-	/* Social network */
-	var SNS_LINKS = [
-		{
-			'name': 'Facebook',
-			'url': 'https://www.facebook.com/orz.zai',
-			'icon': '<i class="fa fa-facebook-square fa-2x"></i>'
-		},
-		{
-			'name': 'Twitter',
-			'url': 'https://twitter.com/intent/tweet?screen_name=Orz_zai',
-			'icon': '<i class="fa fa-twitter fa-2x">'
-		},
-		{
-			'name': 'LinkedIn',
-			'url': 'http://www.linkedin.com/in/jh92710',
-			'icon': '<i class="fa fa-linkedin-square fa-2x"></i>'
-		},
-		{
-			'name': 'GitHub',
-			'url': 'https://github.com/huangginny',
-			'icon': '<i class="fa fa-github fa-2x"></i>'
-		},
-		{
-			'name': 'Flickr',
-			'url': 'http://www.flickr.com/photos/orz_zai/',
-			'icon': '<i class="fa fa-flickr fa-2x"></i>'
-		},
-	];
-
+	/* --- CONTENT --- */
 	ko.applyBindings({'timeline': TIMELINE}, document.getElementById('tlSection'));
 	ko.applyBindings({'links': SNS_LINKS}, document.getElementById('contactSection'));
 
-	/* WHOLE PAGE */
 	$(".section").each(function() {
 		var head = $(this).children('.sectionHead');
 		var body = $(this).children('.sectionBody');
@@ -191,10 +34,9 @@ $(function(){
 		});
 	});
 
-	$(function () {
-  		$('[data-toggle="tooltip"]').tooltip()
-	});
+  	$('[data-toggle="tooltip"]').tooltip()
 
+  	/* --- BOTTOM --- */
 	/*
 	 * From StackOverflow: http://stackoverflow.com/questions/14249998/jquery-back-to-top/#answer-14250025
 	 */
@@ -211,7 +53,7 @@ $(function(){
    		//html works for FFX but not Chrome
    		//body works for Chrome but not FFX
    		//This strange selector seems to work universally
-   		$("html, body").animate({scrollTop: 0}, 1000);
+   		$("html, body").animate({scrollTop: 0});
 	});
 
 });
---
+++ static.js
@@ -0,0 +1,168 @@
+/* Timeline */
+var TIMELINE = [
+{
+	'time': '2014.9',
+	'event': 'Starts internship at Center For Open Science in Charlottesville, VA',
+	'milestone': true,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2014.5',
+	'event': 'Graduates from Washington and Lee University',
+	'milestone': true,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2013.5 - 2013.8',
+	'event': 'Works on Android Automated UI Testing in AmLogic, Inc in '
+	+ 'Santa Clara, CA',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2013.4',
+	'event': 'Goes to Cuba with Christy Cui',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2012.6 - 2012.8',
+	'event': 'Researches on properties of fibonacci numbers as an'
+	+ ' R.E.Lee Scholar, with Professor Dresden and my classmate Cathy',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2011.10',
+	'event': 'Starts work study at Phonathon',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2011.7',
+	'event': 'Movie <i>Harry Potter and the Deathly Hallows - Part 2</i> '
+	+ 'is published, marking the end of the Potter era',
+	'milestone': true,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2011.4',
+	'event': 'Starts my first solo trip in Washington D.C',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2010.8',
+	'event': 'Leaves for United States, and enrolls in Washington and Lee University in Lexington, VA',
+	'milestone': true,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2010.6',
+	'event': 'Graduates from Shenzhen Foreign Languages School',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2009.3',
+	'event': 'Gets 102 on my first TOEFL',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2008.3',
+	'event': 'Co-direct my first short film, <i>Illusion</i>, which '
+	+ 'turns out to be a disaster',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2007.7',
+	'event': 'The book <i>Harry Potter and the Deathly Hallows</i> is published',
+	'milestone': true,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2007.7',
+	'event': 'Screws up my high school entrance exam',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2004.9',
+	'event': 'Starts middle school in Shenzhen Foreign Languages School',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2004.6',
+	'event': 'Graduates from elementary school',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2004.6',
+	'event': 'Passes the test that I have performed most outstandingly in my life',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '1999.?',
+	'event': "Reads <i>Harry Potter and the Philosopher's Stone</i>",
+	'milestone': true,
+	'details': ko.observableArray([])
+},
+{
+	'time': '1998.9',
+	'event': 'Starts elementary school',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '1997.?',
+	'event': 'Talks',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '1994.?',
+	'event': 'Walks',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '1992.7',
+	'event': 'Born',
+	'milestone': true,
+	'details': ko.observableArray([])
+}
+];
+
+/* Social network */
+var SNS_LINKS = [
+{
+	'name': 'Facebook',
+	'url': 'https://www.facebook.com/orz.zai',
+	'icon': '<i class="fa fa-facebook-square fa-2x"></i>'
+},
+{
+	'name': 'Twitter',
+	'url': 'https://twitter.com/intent/tweet?screen_name=Orz_zai',
+	'icon': '<i class="fa fa-twitter fa-2x">'
+},
+{
+	'name': 'LinkedIn',
+	'url': 'http://www.linkedin.com/in/jh92710',
+	'icon': '<i class="fa fa-linkedin-square fa-2x"></i>'
+},
+{
+	'name': 'GitHub',
+	'url': 'https://github.com/huangginny',
+	'icon': '<i class="fa fa-github fa-2x"></i>'
+},
+{
+	'name': 'Flickr',
+	'url': 'http://www.flickr.com/photos/orz_zai/',
+	'icon': '<i class="fa fa-flickr fa-2x"></i>'
+},
+];
