aaedb964a85ace4c27355e0c58253996759484cd
Ginny Huang <sflsginny@hotmail.com>
2014-12-28 19:33:07 -0500

    minor fix

--- minesweeper.js
+++ minesweeper.js
@@ -81,7 +81,7 @@ $(function () {
 			}
 		};
 
-		self.flag = function() {
+		self.flag = function(data, event) {
 			$(this).data("longTapRegistered", true);
 			if (self.clickable()) {
 				self.isFlagged(true);
@@ -90,6 +90,9 @@ $(function () {
 				// if it is already flagged, unflag
 				self.isFlagged(false);
 				parent.numOfFlags(parent.numOfFlags() - 1);
+				if (event.type === 'contextmenu') {
+					$(this).removeData("longTapRegistered");
+				}
 			}
 		};
 	};
