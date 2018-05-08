6ac098bc479ae455e4d6ffb8723d1b8fe2dbd213
Ginny Huang <sflsginny@hotmail.com>
2018-05-08 01:41:02 -0400

    Update

--- static.js
+++ static.js
@@ -1,8 +1,14 @@
 /* Timeline */
 var TIMELINE = [
 {
-	'time': '2016.11',
-	'event': '<i>Fantastic Beasts and Where to Find Them</i> is on in theaters',
+	'time': '2017.11',
+	'event': 'Back in New York as a Video Integration engineer, team mascot, and a party master',
+	'milestone': false,
+	'details': ko.observableArray([])
+},
+{
+	'time': '2017.10',
+	'event': 'Was in Israel for a week',
 	'milestone': false,
 	'details': ko.observableArray([])
 },
@@ -171,13 +177,13 @@ var TIMELINE = [
 /* Facts about me */
 var FACTS = [
 	"I am the biggest Harry Potter fan a lot of people have ever seen.",
-	"My favorite music is usually from movie soundtracks, " + 
-		"such as the opening theme of <i>The Imitation Game</i>",
-	"My favorite city in the world is Hong Kong, and my favorite in the United States " + 
-		"is Washington D.C.",
+	"Favorite music is usually from movie soundtracks.",
+	"Current favorite cuisine is Japanese food.",
 	"I indeed took my English name, \"Ginny\", from the Harry Potter series.",
-	"I own a Nikon D700 and usually make pretty good use of it. Checkout my Flickr!",
-	"I love solo-traveling. once solo-travelled to and couchsurfed Yucatan Peninsula in Mexico.",
+	"A landscape photographer. I used to own a Nikon D700 and now a Sony A7Rii. Checkout my Flickr!",
+	"A globe trotter. Been in all six continents except Antarctica in year 2017: Israel in Asia, " +
+		"Turkey in Europe, the States in North America, Egypt in Africa, Australia in Oceania, " +
+		"and Ecuador in South America in Christmas.",
 	"I always want to have really long hair, but for some reasons they are still always at shoulder length.",
 	"I started college thinking of majoring in Journalism, but quitted because I felt that it " + 
 		"didn't fit me, and also because my English was not good enough.",
@@ -185,29 +191,29 @@ var FACTS = [
 		"take another Math class again. Two years later I became a Math major...",
 	"I play piano. The hardest piece I have ever played is <i>Moonlight Sonata</i> by Beethoven. I presented this " +
 		"piece in my graduation piano recital, in which I performed alongside my good friend Christy.",
-	"Really love to use public transportation for some reason. My favorite is the underground.",
-	"I'm not a sports person. I'm too clumsy...",
+	"Pompeii, Italy is currently on the top of my to-go list. Before Oct 2017 it was Jerusalem, Israel.",
+	"Not a sports person because of my clumsiness, but I like hiking a lot.",
 	"I'm pretty good at Origami. I do this a lot with my girl friends in elementary school.",
-	"I love to buy different kinds of sunglasses.",
-	"I love sending and receiving postcards.",
+	"Loves to buy different kinds of sunglasses.",
+	"Loves sending and receiving postcards.",
 	"The first time I came to the States is in July 2005 with my best friend and a bunch of other " + 
 		"middle school and high school students in Shenzhen. For one week, I was hosted by a family in " + 
 		"Lockhaven, Pennsylvania. That was one of my best trips ever, and I am still missing the time and " + 
 		"people I met when I was there.",
 	"Extremely lazy. On weekends I can just lie on the bed all day.",
-	"I enjoy working in the midnight. For some reason, that's my most efficient working time.",
+	"Enjoys working in the midnight. For some reason, that's my most efficient working time.",
 	"I once drank Coco Cola very much (about half a liter a day.) I can even distinguish Coca Cola and Pepsi, " + 
 		"Coke that's stored in a can and that's stored in a glass bottle. I quitted three months ago, after " +
 		"realizing that it made my stomach hurt a lot.",
 	"On Pottermore (official Harry Potter fansite ran by J.K Rowling) I was sorted into Slytherin.",
 	"I have a habit of always re-watching the movies and TV series I've watched in my childhood. Recently I " + 
 		"am enjoying <i>A Little Princess</i> directed by Alfonso Cuaron in 1995.",
-	"When I talk, I talk a lot. I'm trying to get rid of the bad habit of interrupting others when they talk.",
-	"I don't have a lot of friends. But most of my friends are really close to me.",
+	"Always want to get a tattoo.",
+	"Allergic to lobsters and avocadoes; hates really spicy food.",
 	"When I discover a song that I really love, I would be repeating it for like 20 times a day for a whole " + 
 		"week... that is, until I never want to hear it in my life again. Same goes with dishes of restaurants " + 
 		"I like.",
-	"NA"
+	"Mojito lover. Currently exploring nice speakeasies in NYC."
 ];
 
 /* Social network */
@@ -219,7 +225,7 @@ var SNS_LINKS = [
 },
 {
 	'name': 'Twitter',
-	'url': 'https://twitter.com/intent/tweet?screen_name=Orzzai',
+	'url': 'https://twitter.com/intent/tweet?screen_name=ginnyzr7',
 	'icon': '<i class="fa fa-twitter fa-2x">'
 },
 {
