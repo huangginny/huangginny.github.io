2922e707f14674c3f1b92b4ac6dc72e11fe3245f
Ginny Huang <jh92710@gmail.com>
2021-02-20 04:57:56 -0500

    add static folder for cname

--- src/components/disclaimer.js
+++ src/components/disclaimer.js
@@ -3,7 +3,7 @@ import Box from '@material-ui/core/Box';
 
 const Disclaimer = () => (
 	<Box py={3} fontSize='0.75rem'>
-		<b>disclaimer:</b>: ginsterrific.com and the author gives no guarantee and accept no responsibility
+		<b>disclaimer:</b>: ginsterrific.com and the author give no guarantee and accept no responsibility
 		or liability of the accuracy or the completeness of the materials and information contained.
 		opinions are my own and do not necessarily reflect the views of my employer or any of my affiliations.
 	</Box>
