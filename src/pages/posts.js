2c0405e51a905540da9e9e638ee163c9a9707ba6
Ginny Huang <jh92710@gmail.com>
2021-02-23 15:53:31 -0500

    add time to read

--- src/pages/posts.js
+++ src/pages/posts.js
@@ -2,6 +2,8 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 import { Box, Divider } from '@material-ui/core';
 import { useTheme, makeStyles } from '@material-ui/core/styles';
+import TodayIcon from '@material-ui/icons/Today';
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Disclaimer from "../components/Disclaimer";
@@ -14,14 +16,23 @@ const useStyles = makeStyles(theme => ({
 			borderBottom: `2px solid ${theme.palette.secondary.main}`,
 			boxShadow: 'none'
 		}
+	},
+	icon: {
+		verticalAlign: 'text-bottom',
+		fontSize: '1rem',
 	}
 }));
-const Post = ({slug, title, frontmatter, date, excerpt}) => {
+const Post = ({slug, title, frontmatter, date, readTime, excerpt}) => {
 	const theme = useTheme();
 	const classes = useStyles(theme);
 	return <Box mt={4}>
 		<h3><Link to={slug} className={classes.title}>{title}{" "}</Link></h3>
-		<Box mt={0.8} mb={2.5} color='gray'>{ date.toLowerCase() }</Box>
+		<Box mt={0.8} mb={2.5} color='gray'>
+			<TodayIcon className={classes.icon} />&nbsp;
+			{ date.toLowerCase() }
+			&nbsp;&nbsp;&nbsp;&nbsp;<QueryBuilderIcon className={classes.icon} />&nbsp;
+			{ readTime <= 1 ? '1 min read' : `${ readTime } mins read`}
+		</Box>
 		<p>{ excerpt }</p>
 		<Link to={slug}>read more ></Link>
 		<Box mt={3}><Divider /></Box>
@@ -38,6 +49,7 @@ const Posts = ({data}) => (
 			slug={ node.fields.slug }
 			title={ node.frontmatter.title }
 			date={ node.frontmatter.date }
+			readTime={ node.timeToRead }
 			excerpt={ node.excerpt } 
 		/>
 	))}
@@ -57,6 +69,7 @@ export const query = graphql`
 			edges {
 				node {
 					id
+					timeToRead
 					frontmatter {
 						title
 						date(formatString: "MMMM DD, YYYY")
--- src/templates/article.js
+++ src/templates/article.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { graphql } from "gatsby";
+import { makeStyles } from '@material-ui/core/styles';
+import TodayIcon from '@material-ui/icons/Today';
+import QueryBuilderIcon from '@material-ui/icons/QueryBuilder';
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Disclaimer from "../components/Disclaimer";
 import Paginator from "../components/Paginator";
 
+const useStyles = makeStyles({
+	subtitle: { color: 'grey' },
+	icon: {
+		verticalAlign: 'text-bottom',
+		fontSize: '1rem',
+	}
+});
+
 export default function Article({ data, pageContext }) {
+	const classes = useStyles();
+
 	const post = data.markdownRemark;
 	return (
 		<Layout>
 			<SEO title={post.frontmatter.title} />
 			<div>
 				<h1>{post.frontmatter.title}</h1>
-				<p style={{color: 'grey'}}>{post.frontmatter.date.toLowerCase()}</p>
+				<p className={classes.subtitle}>
+					<TodayIcon className={classes.icon} />&nbsp;
+					{post.frontmatter.date.toLowerCase()}
+					&nbsp;&nbsp;&nbsp;&nbsp;<QueryBuilderIcon className={classes.icon} />&nbsp;
+					{post.timeToRead <= 1 ? '1 min read' : `${post.timeToRead} mins read`}
+				</p>
 			<div dangerouslySetInnerHTML={{ __html: post.html }} /></div>
 			<Paginator
 				prev={ pageContext.prev }
@@ -29,6 +47,7 @@ export const query = graphql`
 	query($slug: String!) {
 		markdownRemark(fields: { slug: { eq: $slug } }) {
 			html
+			timeToRead
 			frontmatter {
 				title
 				date(formatString: "MMMM DD, YYYY")
